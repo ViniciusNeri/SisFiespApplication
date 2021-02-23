ed194a3a3b6c3e086742bbd62bb0f08b3fe3db7f
Vinicius Ribeiro Neri <viniciusneri7@gmail.com>
2021-02-22 22:23:49 -0300

    Primeiro commit

---
+++ SisFiespApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SisFiespApplication.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ SisFiespApplication/Models/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SisFiespApplication.Models
+{
+	[Table("Usuario")]
+	public class Usuario
+	{
+		[Key]
+		[Display(Name = "Codigo")]
+		[Column("Codigo")]
+		public int Codigo { get; set; }
+
+		[Display(Name = "Login")]
+		[Column("Login")]
+		public int Login { get; set; }
+
+		[Display(Name = "Senha")]
+		[Column("Senha")]
+		public int Senha { get; set; }
+
+		[Display(Name = "Email")]
+		[Column("Email")]
+		public int Email { get; set; }
+	}
+}
