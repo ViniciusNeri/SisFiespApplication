c6cf3f38b12fdb8d2f0a4e1328e3cb522ffebc20
Vinicius Ribeiro Neri <viniciusneri7@gmail.com>
2021-03-06 11:44:28 -0300

    alter

--- SisFiespApplication/wwwroot/js/aluno.js
+++ SisFiespApplication/wwwroot/js/aluno.js
@@ -201,14 +201,10 @@ function salvarAlunoEdit() {
 		dados += '{name:"Observacao", value:"' + $('#observacaoAluno').val() + '"},';
 	}
 	if ($('#idAluno').val() != "") {
-		dados += '{name:"Codigo", value:"' + $('#idAluno').val() + '"},'  ; 
+		dados += '{name:"Codigo", value:"' + $('#idAluno').val() + '"},'; 
 	}
 	dadosEnvio = eval("[" + dados + "]");
-
-	/*
-	 * teste
-	 * */
-
+	
 	$.ajax({
 		url: "/Alunos/Edit",
 		method: "POST",
